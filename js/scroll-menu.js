75b9decc21a7dba26f2b44d43d366df23811c080
Bartek Pieła <Bartek-Macbook@MacBook-Pro-Bartek.local>
2016-10-25 19:31:19 +0200

    1 push

---
+++ js/scroll-menu.js
@@ -0,0 +1,4 @@
+function scroll_to(selector) {
+    $('html,body').animate({scrollTop: $(selector).offset().top}, 1000);
+    return false;
+}
---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.circle').click(function () {
+        $('.second').toggle();
+    });
+
+})
